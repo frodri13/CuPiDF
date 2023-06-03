87a5c0edc543446f36fd5af2a5ee81f1ea2ce454
Fabio Rodriguez <alessf13@gmail.com>
2023-06-03 05:10:59 +0100

    DarkMode implemented

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import DarkModeToggle from '@/components/DarkModeToggle'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -15,7 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} dark:bg-black`}>
+        <DarkModeToggle />
+        {children}
+        </body>
     </html>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundColor: ['dark'],
+      textColor: ['dark'],
+      borderColor: ['dark'],
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
@@ -15,4 +18,5 @@ module.exports = {
     },
   },
   plugins: [],
+  darkMode: 'class'
 }
